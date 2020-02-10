809d1e1b2090338af2b1b710f25c63bb5c6c94db
Moaid Hathot <moaid.hathot@outlook.com>
2020-02-10 14:38:31 +0200

    commit

---
+++ Code.Digest.Bot/Code.Digest.Bot/TwitterCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code.Digest.Bot
+{
+    public class TwitterCache
+    {
+        public string LastId { get; set; }
+    }
+}
