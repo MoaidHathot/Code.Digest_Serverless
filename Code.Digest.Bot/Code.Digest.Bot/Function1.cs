4dc5afbb2953d41b2a080890b33861ec7ae5350b
Moaid Hathot <moaid.hathot@outlook.com>
2020-02-10 14:45:12 +0200

    code

--- Code.Digest.Bot/Code.Digest.Bot/Function1.cs
+++ Code.Digest.Bot/Code.Digest.Bot/Function1.cs
@@ -15,44 +15,13 @@ namespace Code.Digest.Bot
     public static class Function1
     {
         [FunctionName(nameof(FetchTweetsTimer))]
-        public static async Task FetchTweetsTimer([TimerTrigger("0 * * * * *")]TimerInfo timerInfo, ILogger log, [Blob("twitter-cache/lastExecution.json", FileAccess.ReadWrite)] CloudBlockBlob cache)
+        public static void FetchTweetsTimer([TimerTrigger("0 * * * * *")]TimerInfo timerInfo, ILogger log)
         {
             var consumerKey = Environment.GetEnvironmentVariable("ConsumerKey");
             var consumerSecret = Environment.GetEnvironmentVariable("ConsumerSecret");
             var accessToken= Environment.GetEnvironmentVariable("Accesstoken");
             var accessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret");
-
-            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-
-            var searchTerm = Environment.GetEnvironmentVariable("SearchTerm");
-
-            long lastId = default;
-
-            if (await cache.ExistsAsync())
-            {
-                lastId = long.Parse(await cache.DownloadTextAsync());
-            }
-
-            var searchArgs = new SearchTweetsParameters(searchTerm)
-            {
-                SinceId = lastId
-            };
-
-            var tweets = await SearchAsync.SearchTweets(searchArgs);
-
-
-            if (tweets.Any())
-            {
-                lastId = tweets.Max(tweet => tweet.Id);
-                await cache.UploadTextAsync(lastId.ToString());
-
-            }
-
-            foreach (var tweet in tweets)
-            {
-                Tweet.PublishRetweet(tweet.Id);
-                log.LogInformation("Published {tweet} - {id} - {name}", tweet.Url, tweet.Id, tweet.CreatedBy.ScreenName);
-            }
+        
         }
     }
 }
